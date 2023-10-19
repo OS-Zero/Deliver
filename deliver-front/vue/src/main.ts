2306ea14e1cf3a38677a4246b35b324f59457b7b
hzhfsa <1957214281@qq.com>
2023-10-19 10:07:00 +0800

    test

--- deliver-front/vue/src/main.ts
+++ deliver-front/vue/src/main.ts
@@ -6,6 +6,9 @@ import '@/style/global.less';
 import i18n from '@/locales/useI18n'
 import 'virtual:svg-icons-register';
 import setupDefaultSetting from '@/utils/setupDefaultSetting'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 import Antd from 'ant-design-vue';
 // 用来切换主题
@@ -17,6 +20,7 @@ app.use(Antd)
 app.use(router)
 app.use(store)
 app.use(i18n)
+app.use(ElementPlus)
 app.mount('#app');
 
 setupDefaultSetting()
