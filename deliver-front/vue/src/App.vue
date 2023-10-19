d2d4fa44b5ddbf8b6f6d28b3a605415e308c65e4
dengyu <3233891353@qq.com>
2023-10-19 20:07:35 +0800

    重新创建vue项目

--- deliver-front/vue/src/App.vue
+++ deliver-front/vue/src/App.vue
@@ -1,45 +1,31 @@
-<template>
-  <a-config-provider :locale="lang[proxy.$i18n.locale]">
-    <router-view />
-  </a-config-provider>
-  <LockScreen />
-</template>
-
-<script lang="ts" setup name="App">
-import { getCurrentInstance, onErrorCaptured, h } from 'vue'
-import zh_CN from 'ant-design-vue/es/locale/zh_CN'
-import en_US from 'ant-design-vue/lib/locale-provider/en_US'
-import { setDeviceType } from '@/utils/device'
-import LockScreen from '@/components/LockScreen/index.vue'
-import emitter from '@/utils/eventBus'
-import { useRouter } from 'vue-router'
-import { Modal } from 'ant-design-vue'
-
-const { proxy } = getCurrentInstance()
-const lang = { 'en-US': en_US, 'zh-CN': zh_CN }
-
-window.onresize = setDeviceType
-setDeviceType()
-
-const router = useRouter()
-emitter.once('axios_goto_login', () => {
-  router.push({ name: 'login' })
-})
-
-//全局错误处理
-onErrorCaptured((err, instance, info) => {
-  if (window.env !== 'localhost') {
-    // debugger
-    console.log(err, instance, info)
-    Modal.error({
-      title: 'System Error',
-      content: h('pre', err.stack),
-      class: 'errorCapturedModal',
-      width: '60%'
-    })
-  }
-})
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+let a = 1
 </script>
 
-<style>
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
