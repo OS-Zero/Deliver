059167d92a868dae851be9d8cb1e2d8cc1e9aace
ruochu995 <2308850421@qq.com>
2023-10-23 14:46:04 +0800

    refactor(backend) sql 库表设置 修改 MySQL sql 初始化，表结构修改

--- deliver-backend/db/mysql/init.sql
+++ deliver-backend/db/mysql/init.sql
@@ -14,7 +14,7 @@ create table if not exists deliver.template
     push_ways     varchar(200)                                                   not null comment '推送方式
 {
 "channelType":（1-打电话 2-发短信 3-邮件 4-企业微信 5-钉钉 6-飞书）
-"messageType": 参考
+"messageType": 参考 MessageTypeEnum 的 code
 }',
     use_count     int      default 0                                             not null comment '模板使用数',
     create_user   varchar(50)                                                    null comment '创建者',
@@ -30,7 +30,6 @@ create table if not exists deliver.app
 (
     app_id       bigint auto_increment primary key comment 'appId',
     name         varchar(100)                                                   not null comment '应用名称',
-    channel_type tinyint                                                        not null comment '消息发送渠道类型 （1-打电话 2-发短信 3-邮件 4-企业微信 5-钉钉 6-飞书）',
     app_config   text                                                           not null comment '应用信息配置 json',
     use_count    int      default 0                                             not null comment 'APP 使用数',
     create_user  varchar(50)                                                    null comment '创建者',
