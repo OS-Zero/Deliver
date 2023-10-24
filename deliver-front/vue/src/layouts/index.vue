88c216318756d523ba48822d6decec82ec5ec0f0
dengyu <3233891353@qq.com>
2023-10-24 11:31:41 +0800

    feat:layout build

---
+++ deliver-front/vue/src/layouts/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import Header from './components/Header/index.vue'
+import SideBar from './components/SideBar/index.vue'
+import Breadcrumb from './components/Breadcrumb/index.vue'
+</script>
+
+<template>
+  <a-layout style="height: 100%">
+    <Header></Header>
+    <a-layout>
+      <SideBar></SideBar>
+      <a-layout style="padding: 0 12px 12px">
+        <Breadcrumb></Breadcrumb>
+        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+          <RouterView></RouterView>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<style scoped lang="sass"></style>
