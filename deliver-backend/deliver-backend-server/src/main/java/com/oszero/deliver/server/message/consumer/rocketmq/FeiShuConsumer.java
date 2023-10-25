04d6bb313bcbf8b7ecf670be9f9554dcca3e9489
ruochu995 <2308850421@qq.com>
2023-10-26 00:09:29 +0800

    feature(server): 消费重试机制 RocketMQ 飞书消费增加重试机制。

--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/consumer/rocketmq/FeiShuConsumer.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/consumer/rocketmq/FeiShuConsumer.java
@@ -3,6 +3,7 @@ package com.oszero.deliver.server.message.consumer.rocketmq;
 import cn.hutool.json.JSONUtil;
 import com.oszero.deliver.server.constant.MQConstant;
 import com.oszero.deliver.server.message.consumer.handler.impl.FeiShuHandler;
+import com.oszero.deliver.server.message.producer.Producer;
 import com.oszero.deliver.server.model.dto.SendTaskDto;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -14,15 +15,21 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-@RocketMQMessageListener(topic = MQConstant.FEI_SHU_TOPIC, consumerGroup = MQConstant.FEI_SHU_CONSUMER_GROUP)
+@RocketMQMessageListener(
+        topic = MQConstant.FEI_SHU_TOPIC,
+        consumerGroup = MQConstant.FEI_SHU_CONSUMER_GROUP
+)
 @ConditionalOnProperty(value = "mq-type", havingValue = "rocketmq")
 public class FeiShuConsumer implements RocketMQListener<MessageExt> {
 
     private final FeiShuHandler feiShuHandler;
+
+    private final Producer producer;
 
     /**
      * 没有报错，就签收
@@ -34,9 +41,17 @@ public class FeiShuConsumer implements RocketMQListener<MessageExt> {
     @Override
     public void onMessage(MessageExt messageExt) {
         log.info("[MailConsumer 接收到消息] {}", messageExt);
-        SendTaskDto sendTaskDto = JSONUtil.toBean(
-                new String(messageExt.getBody(), StandardCharsets.UTF_8
-                ), SendTaskDto.class);
-        feiShuHandler.doHandle(sendTaskDto);
+        SendTaskDto sendTaskDto = null;
+        try {
+            sendTaskDto = JSONUtil.toBean(
+                    new String(messageExt.getBody(), StandardCharsets.UTF_8
+                    ), SendTaskDto.class);
+            feiShuHandler.doHandle(sendTaskDto);
+        } catch (Exception exception) {
+            if (!Objects.isNull(sendTaskDto) && sendTaskDto.getRetry() > 0) {
+                sendTaskDto.setRetry(sendTaskDto.getRetry() - 1);
+                producer.sendMessage(sendTaskDto);
+            }
+        }
     }
 }
