e428b20a908c3ed4a1388e252435f0be5096b2c9
ruochu995 <2308850421@qq.com>
2023-10-27 17:57:29 +0800

    feature(admin): 新建仪表盘 Controller 管理端新建 DashboardController

---
+++ deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package com.oszero.deliver.admin.controller;
+
+import com.oszero.deliver.admin.service.AppService;
+import com.oszero.deliver.admin.service.TemplateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 仪表盘
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final AppService appService;
+    private final TemplateService templateService;
+}
