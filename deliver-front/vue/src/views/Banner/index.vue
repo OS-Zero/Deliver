7dec06b1c7b9d8dd6c7b8208aff3eb91109e30fd
jin-zhenbang <1660961170@qq.com>
2023-11-02 21:12:59 +0800

    style:横幅样式修改

--- deliver-front/vue/src/views/Banner/index.vue
+++ deliver-front/vue/src/views/Banner/index.vue
@@ -1,74 +1,35 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="banner">
-    <p>
-      <svg
-        class="ico"
-        :style="{ marginRight: '5px' }"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 32 32"
-        width="24"
-        height="24"
-        style="border-color: rgba(187, 187, 187, 1); border-width: 0px; border-style: solid"
-        filter="none"
-      >
-        <g>
-          <path
-            d="M16 0.667l5.635 8.244 9.583 2.812-6.1 7.907 0.287 9.981-9.404-3.357-9.404 3.357 0.287-9.981-6.1-7.907 9.583-2.812 5.635-8.244zM13.333 16h-2.667c0 0 0 0 0 0 0 2.946 2.388 5.333 5.333 5.333 2.852 0 5.181-2.238 5.326-5.054l0.001-0.013 0.007-0.267h-2.667c0 0.004 0 0.008 0 0.012 0 1.473-1.194 2.667-2.667 2.667-1.407 0-2.559-1.089-2.66-2.47l-0.001-0.009-0.007-0.2z"
-            fill="rgba(251.94,201.96,0,1)"
-          ></path>
-        </g>
-      </svg>
-      如果你喜欢 Deliver 消息推送平台，请给它一个 Star Gitee,你的支持将是我们前行的动力，项目正在积极开发，欢迎大家提交
-      PR、提供建设,共建社区生态
-      <svg
-        class="ico"
-        :style="{ marginLeft: '5px' }"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 48 48"
-        width="24"
-        height="24"
-        style="border-color: rgba(187, 187, 187, 1); border-width: 0px; border-style: solid"
-        filter="none"
-      >
-        <g>
-          <path
-            d="M27.9467 16.8281L40.7568 7.73711C41.6731 7.08684 42.9535 7.40693 43.4559 8.4119C43.7856 9.07125 43.688 9.86396 43.2083 10.4237L41 13L35.7241 20.0345C35.2788 20.6283 35.1989 21.4199 35.5166 22.0907L42.6471 37.1438C43.2756 38.4707 42.3078 40 40.8396 40H32.6363C31.6844 40 30.8645 39.3291 30.676 38.3961L27.5197 22.7739L27.1139 18.6553C27.0435 17.9409 27.3613 17.2436 27.9467 16.8281Z"
-            stroke="rgba(233.07000000000002,157.07999999999998,66.045,1)"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            fill="none"
-          ></path>
-          <path
-            d="M28 23L35 21"
-            stroke="rgba(233.07000000000002,157.07999999999998,66.045,1)"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            fill="none"
-          ></path>
-          <path
-            d="M19.9359 17.1028L7.20861 8.19574C6.30625 7.56424 5.05387 7.87714 4.55456 8.85884C4.21946 9.51768 4.31813 10.314 4.8039 10.8711L7 13.3898L12.2323 20.0262C12.7026 20.6227 12.7943 21.4342 12.4692 22.1207L5.35292 37.1438C4.7244 38.4707 5.69217 40 7.1604 40H15.322C16.2925 40 17.1229 39.3033 17.2916 38.3476L20 23L20.7526 19.1224C20.903 18.347 20.5831 17.5557 19.9359 17.1028Z"
-            stroke="rgba(233.07000000000002,157.07999999999998,66.045,1)"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            fill="none"
-          ></path>
-          <path
-            d="M13 21L20 23"
-            stroke="rgba(233.07000000000002,157.07999999999998,66.045,1)"
-            stroke-width="4"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            fill="none"
-          ></path>
-        </g>
-      </svg>
-    </p>
+  <div
+    data-v-05bfc04b=""
+    class="ant-alert ant-alert-info ant-alert-no-icon ant-alert-banner css-190m0jy"
+    data-show="true"
+    role="alert"
+    style="
+      padding: 0px;
+      background: repeating-linear-gradient(
+        35deg,
+        rgb(179, 235, 255),
+        rgb(179, 235, 255) 20px,
+        rgb(230, 248, 255) 10px,
+        rgb(230, 248, 255) 40px
+      );
+    "
+  >
+    <div data-v-05bfc04b="" class="ant-alert-content">
+      <div data-v-05bfc04b="" class="ant-alert-message">
+        <div
+          data-v-05bfc04b=""
+          style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; text-align: center; color: black"
+        >
+          <span data-v-05bfc04b="">⭐️ 如果你喜欢 Deliver 消息推送平台，请给它一个 Star </span
+          ><a data-v-05bfc04b="" target="_blank" rel="noopener noreferrer" href="https://gitee.com/topiam/eiam">Gitee</a
+          ><span data-v-05bfc04b=""
+            >,你的支持将是我们前行的动力，项目正在积极开发，欢迎大家提交 PR、提供建设,共建社区生态。👏🏻</span
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
