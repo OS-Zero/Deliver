f29d3392c2f3bb73e48fe1be6435b935b665159d
ruochu995 <2308850421@qq.com>
2023-11-03 16:58:54 +0800

    feature(admin): 添加消息记录 MessageRecord
    增加实体类，mapper 接口、xml

---
+++ deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/mapper/MessageRecordMapper.java
@@ -0,0 +1,18 @@
+package com.oszero.deliver.admin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.oszero.deliver.admin.model.entity.MessageRecord;
+
+/**
+ * 针对表【message_record(消息记录)】的数据库操作Mapper
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
+public interface MessageRecordMapper extends BaseMapper<MessageRecord> {
+
+}
+
+
+
+
