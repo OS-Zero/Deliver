f0208cb82c30def4cab31b0fe5a75afa79e66752
ruochu995 <2308850421@qq.com>
2023-11-07 20:31:59 +0800

    feature(admin): 数据面板消息信息接口完成
    根据具体的选择返回不同的值

---
+++ deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/enums/DateSelectEnum.java
@@ -0,0 +1,33 @@
+package com.oszero.deliver.admin.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * 日期选择枚举
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
+@Getter
+@ToString
+@AllArgsConstructor
+public enum DateSelectEnum {
+
+    DAY(1, "今日"),
+    WEEK(2, "本周"),
+    MONTH(3, "本月"),
+    YEAR(4, "今年");
+    private final Integer code;
+    private final String name;
+
+    public static DateSelectEnum getInstanceByCode(Integer code) {
+        for (DateSelectEnum v : values()) {
+            if (v.getCode().equals(code)) {
+                return v;
+            }
+        }
+        return null;
+    }
+}
--- deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/service/DashboardService.java
+++ deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/service/DashboardService.java
@@ -3,6 +3,12 @@ package com.oszero.deliver.admin.service;
 import com.oszero.deliver.admin.model.dto.request.DashboardDateSelectRequestDto;
 import com.oszero.deliver.admin.model.dto.response.*;
 
+/**
+ * 仪表盘 service
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
 public interface DashboardService {
     DashboardHeadDataResponseDto getDashboardHeadData();
 
