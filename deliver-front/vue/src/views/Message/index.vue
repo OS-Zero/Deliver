91930162b122fb8199d82a87fe75bc88fb7dfce0
hzhfsa <1957214281@qq.com>
2023-11-08 23:01:15 +0800

    feat:更新表格

--- deliver-front/vue/src/views/Message/index.vue
+++ deliver-front/vue/src/views/Message/index.vue
@@ -1,145 +1,170 @@
 <script lang="ts" setup>
 import { ReloadOutlined } from '@ant-design/icons-vue'
-import { ref, reactive, h, watch, computed } from 'vue'
+import { ref, reactive, h, onMounted } from 'vue' // watch, computed,
 import type { UnwrapRef } from 'vue'
+// import type { Rule } from 'ant-design-vue/es/form'
 import type { TableColumnsType } from 'ant-design-vue'
-import type { Rule } from 'ant-design-vue/es/form'
-import type { messageTemplate } from './type'
+import type { messageTemplate, searchMessage } from './type'
 import searchForm from './components/searchForm.vue'
+import { getTemplatePages } from '@/api/message'
+import { getDate } from '@/utils/date'
 // import { message } from 'ant-design-vue'
 
-/// 表格部分
+/// 增删改查
+const searchform = ref()
 // 新增
 const open = ref<boolean>(false)
 const addModules = (): void => {
   open.value = true
 }
 
-const labelCol = { span: 4 }
+// const labelCol = { span: 4 }
 
-const wrapperCol = { span: 20 }
+// const wrapperCol = { span: 20 }
+// const sea = ref()
 
-const addTemplate: UnwrapRef<messageTemplate> = reactive({
-  templateName: '',
-  pushRange: '',
-  userType: '',
-  channel: '',
-  messageType: '',
-  channelApp: ''
+const templateTable: UnwrapRef<messageTemplate[]> = reactive([])
+
+const searchItem: searchMessage = reactive({
+  templateName: undefined,
+  pushRange: undefined,
+  usersType: undefined,
+  currentPage: 1,
+  pageSize: 10
 })
 
-const rules: Record<string, Rule[]> = {
-  templateName: [
-    { required: true, message: '请输入模板名', trigger: 'change' },
-    { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
-  ],
-  pushRange: [{ required: true, message: '请选择推送范围', trigger: 'change' }],
-  userType: [{ required: true, message: '请选择用户类型', trigger: 'change' }],
-  messageType: [
-    {
-      required: true,
-      message: '请选择消息类型',
-      trigger: 'change'
-    }
-  ],
-  channel: [
-    {
-      required: true,
-      message: '请选择渠道',
-      trigger: 'change'
-    }
-  ],
-  channelApp: [{ required: true, message: '请选择渠道App', trigger: 'change' }]
-}
-
-const templateForm = ref()
-const handleOk = (): void => {
-  templateForm.value
-    .validate()
-    .then(() => {
-      console.log('values', addTemplate)
-      open.value = false
+const searchTemplate = (): void => {
+  getTemplatePages(searchItem)
+    .then(res => {
+      templateTable.length = 0
+      if (res.records.length > 0) {
+        res.records.forEach((item: any, index: number) => {
+          item.channelType = JSON.parse(item.pushWays).channelType
+          item.messageType = JSON.parse(item.pushWays).messageType
+          item.createTime = getDate(item.createTime)
+          item.key = index
+          templateTable.push(item)
+        })
+        console.warn(templateTable)
+      }
     })
-    .catch(error => {
-      console.log('error', error)
+    .catch(err => {
+      console.error('An error occurred:', err)
     })
 }
-const handleCancel = (): void => {
-  templateForm.value.resetFields()
-}
+// const rules: Record<string, Rule[]> = {
+//   templateName: [
+//     { required: true, message: '请输入模板名', trigger: 'change' },
+//     { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+//   ],
+//   pushRange: [{ required: true, message: '请选择推送范围', trigger: 'change' }],
+//   usersType: [{ required: true, message: '请选择用户类型', trigger: 'change' }],
+//   messageType: [
+//     {
+//       required: true,
+//       message: '请选择消息类型',
+//       trigger: 'change'
+//     }
+//   ],
+//   channel: [
+//     {
+//       required: true,
+//       message: '请选择渠道',
+//       trigger: 'change'
+//     }
+//   ],
+//   channelApp: [{ required: true, message: '请选择渠道App', trigger: 'change' }]
+// }
 
-const channelData = ['默认标签1', '默认标签2', '默认标签3']
+// const templateForm = ref()
+// const handleOk = (): void => {
+//   templateForm.value
+//     .validate()
+//     .then(() => {
+//       console.log('values', templateTable)
+//       open.value = false
+//     })
+//     .catch(error => {
+//       console.log('error', error)
+//     })
+// }
+// const handleCancel = (): void => {
+//   templateForm.value.resetFields()
+// }
 
-const messageData = {
-  默认标签1: ['Online'],
-  默认标签2: ['Promotion'],
-  默认标签3: ['Offline']
-}
+// const channelData = ['默认标签1', '默认标签2', '默认标签3']
 
-const appData = {
-  默认标签1: ['IOS'],
-  默认标签2: ['安卓'],
-  默认标签3: ['MAC']
-}
+// const messageData = {
+//   默认标签1: ['Online'],
+//   默认标签2: ['Promotion'],
+//   默认标签3: ['Offline']
+// }
 
-const mesType = computed(() => {
-  return messageData[addTemplate.channel] === undefined ? [] : messageData[addTemplate.channel]
-})
+// const appData = {
+//   默认标签1: ['IOS'],
+//   默认标签2: ['安卓'],
+//   默认标签3: ['MAC']
+// }
 
-const appType = computed(() => {
-  return appData[addTemplate.channel] === undefined ? [] : appData[addTemplate.channel]
-})
+// const mesType = computed(() => {
+//   return messageData[templateTable.channel] === undefined ? [] : messageData[templateTable.channel]
+// })
 
-watch(
-  () => addTemplate.channel,
-  newVal => {
-    if (messageData[newVal] !== undefined) {
-      addTemplate.messageType = messageData[newVal][0]
-    } else {
-      // 未定义或空数组的情况
-      addTemplate.messageType = ''
-      messageData[newVal] = []
-    }
-    if (appData[newVal] !== undefined) {
-      addTemplate.channelApp = appData[newVal][0]
-    } else {
-      // 处理未定义或空数组的情况
-      addTemplate.channelApp = ''
-      appData[newVal] = []
-    }
-  }
-)
+// const appType = computed(() => {
+//   return appData[templateTable.channel] === undefined ? [] : appData[templateTable.channel]
+// })
 
+// watch(
+//   () => templateTable.channel,
+//   newVal => {
+//     if (messageData[newVal] !== undefined) {
+//       templateTable.messageType = messageData[newVal][0]
+//     } else {
+//       // 未定义或空数组的情况
+//       templateTable.messageType = ''
+//       messageData[newVal] = []
+//     }
+//     if (appData[newVal] !== undefined) {
+//       templateTable.channelApp = appData[newVal][0]
+//     } else {
+//       // 处理未定义或空数组的情况
+//       templateTable.channelApp = ''
+//       appData[newVal] = []
+//     }
+//   }
+// )
+
+// 表格数据
 const columns: TableColumnsType = [
   {
     title: '模版 ID',
-    dataIndex: 'projectName',
-    key: 'projectname'
+    dataIndex: 'templateId',
+    key: 'templateId'
   },
   {
     title: '模板名',
-    dataIndex: 'admin1',
-    key: 'admin1'
+    dataIndex: 'templateName',
+    key: 'templateName'
   },
   {
     title: '推送范围',
-    dataIndex: 'admin2',
-    key: 'admin2'
+    dataIndex: 'pushRange',
+    key: 'pushRange'
   },
   {
     title: '用户类型',
-    dataIndex: 'admin2',
-    key: 'admin2'
+    dataIndex: 'usersType',
+    key: 'usersType'
   },
   {
     title: '模板累计使用数',
-    dataIndex: 'admin2',
-    key: 'admin2'
+    dataIndex: 'useCount',
+    key: 'useCount'
   },
   {
     title: '模板状态',
-    dataIndex: 'admin2',
-    key: 'admin2'
+    dataIndex: 'templateStatus',
+    key: 'templateStatus'
   },
   {
     title: '操作',
@@ -148,20 +173,81 @@ const columns: TableColumnsType = [
     width: 180
   }
 ]
-// data到时候肯定是需要去接口获取到res然后渲染的
-const data = ref([
+
+const innerColumns = [
   {
-    key: '1',
-    projectName: '1.txt',
-    admin1: 'a',
-    admin2: 'b'
+    title: '渠道选择',
+    dataIndex: 'channelType',
+    key: 'channelType'
+  },
+  {
+    title: '消息类型',
+    dataIndex: 'messageType',
+    key: 'messageType'
+  },
+  {
+    title: '创建用户',
+    dataIndex: 'createUser',
+    key: 'createUser'
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    key: 'createTime'
+  },
+  {
+    title: '渠道App ID',
+    dataIndex: 'appId',
+    key: 'appId'
+  },
+  {
+    title: '渠道App名称',
+    dataIndex: 'appName',
+    key: 'appName'
   }
-])
+]
+
+// 渲染 data
+const innertemplatedata: UnwrapRef<messageTemplate[]> = reactive([])
+
+const expandedRowKeys: number[] = reactive([])
+
+const getInnerData = (expanded, record): void => {
+  // 判断是否点开
+  expandedRowKeys.length = 0
+  if (expanded === true) {
+    const b = record.key.toString()
+    expandedRowKeys.push(Number(b.slice(-1)))
+    innertemplatedata.length = 0
+    innertemplatedata.push(record)
+  } else {
+    expandedRowKeys.length = 0
+  }
+}
+
+onMounted(() => {
+  getTemplatePages(searchItem)
+    .then(res => {
+      if (res.records.length > 0) {
+        res.records.forEach((item: any, index: number) => {
+          item.channelType = JSON.parse(item.pushWays).channelType
+          item.messageType = JSON.parse(item.pushWays).messageType
+          item.createTime = getDate(item.createTime)
+          item.key = index
+          templateTable.push(item)
+        })
+        console.warn(templateTable)
+      }
+    })
+    .catch(err => {
+      console.error('An error occurred:', err)
+    })
+})
 </script>
 
 <template>
   <!-- 搜索部分 -->
-  <searchForm />
+  <searchForm ref="searchform" @searchMes="searchTemplate" />
   <!-- 表格部分 -->
   <div id="message-container">
     <div class="message-section">
@@ -171,30 +257,20 @@ const data = ref([
         </a-tooltip>
         <a-button type="primary" class="addModule" @click="addModules">新增模板</a-button>
         <a-modal v-model:open="open" title="新增模板" width="650px" :footer="null" @cancel="handleCancel">
-          <a-form
-            ref="templateForm"
-            :model="addTemplate"
-            :rules="rules"
-            :label-col="labelCol"
-            :wrapper-col="wrapperCol"
-            class="temform"
-          >
+          <!-- <a-form ref="templateForm" :model="templateTable" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol"
+            class="temform">
             <a-form-item ref="templateName" label="模板名" name="templateName" class="tem-item">
-              <a-input
-                v-model:value="addTemplate.templateName"
-                placeholder="请填写长度在3到20个字符的模板名"
-                style="width: 70%"
-              />
+              <a-input v-model:value="templateTable.templateName" placeholder="请填写长度在3到20个字符的模板名" style="width: 70%" />
             </a-form-item>
             <a-form-item label="推送范围" name="pushRange" class="tem-item">
-              <a-radio-group v-model:value="addTemplate.pushRange" button-style="solid">
+              <a-radio-group v-model:value="templateTable.pushRange" button-style="solid">
                 <a-radio-button :value="0">不限</a-radio-button>
                 <a-radio-button :value="1">企业内部</a-radio-button>
                 <a-radio-button :value="2">企业外部</a-radio-button>
               </a-radio-group>
             </a-form-item>
-            <a-form-item label="用户类型" name="userType" class="tem-item">
-              <a-radio-group v-model:value="addTemplate.userType" button-style="solid">
+            <a-form-item label="用户类型" name="usersType" class="tem-item">
+              <a-radio-group v-model:value="templateTable.usersType" button-style="solid">
                 <a-radio-button :value="1">企业账号</a-radio-button>
                 <a-radio-button :value="2">电话</a-radio-button>
                 <a-radio-button :value="3">邮箱</a-radio-button>
@@ -202,41 +278,55 @@ const data = ref([
               </a-radio-group>
             </a-form-item>
             <a-form-item label="渠道选择" name="channel" class="tem-item">
-              <a-select
-                v-model:value="addTemplate.channel"
-                :options="channelData.map(pro => ({ value: pro }))"
-                style="width: 70%"
-              />
+              <a-select v-model:value="templateTable.channel" :options="channelData.map(pro => ({ value: pro }))"
+                style="width: 70%" />
             </a-form-item>
             <a-form-item label="消息类型" name="messageType" class="tem-item">
-              <a-select
-                v-model:value="addTemplate.messageType"
-                :options="mesType.map((pro: any) => ({ value: pro }))"
-                style="width: 70%"
-              />
+              <a-select v-model:value="templateTable.messageType" :options="mesType.map((pro: any) => ({ value: pro }))"
+                style="width: 70%" />
             </a-form-item>
             <a-form-item label="渠道App" name="channelApp" class="tem-item">
-              <a-select
-                v-model:value="addTemplate.channelApp"
-                :options="appType.map((pro: any) => ({ value: pro }))"
-                style="width: 70%"
-              />
+              <a-select v-model:value="templateTable.channelApp" :options="appType.map((pro: any) => ({ value: pro }))"
+                style="width: 70%" />
             </a-form-item>
             <a-form-item :wrapper-col="{ span: 14, offset: 4 }" class="tem-item">
               <a-button type="primary" @click="handleOk">确认新建</a-button>
               <a-button style="margin-left: 10px" @click="handleCancel">重置</a-button>
             </a-form-item>
-          </a-form>
+          </a-form> -->
         </a-modal>
       </div>
 
       <!-- 表格部分 -->
-      <a-table :columns="columns" :data-source="data" :scroll="{ x: 1200, y: 300 }" bordered>
-        <template #bodyCell="{ column }">
+      <a-table
+        :columns="columns"
+        :data-source="templateTable"
+        :scroll="{ x: 1200, y: 900 }"
+        bordered
+        class="components-table-demo-nested"
+        @expand="getInnerData"
+        :expandedRowKeys="expandedRowKeys"
+      >
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'templateStatus'">
+            <span v-if="record.templateStatus === 1">
+              <a-button type="primary" style="background-color: rgb(102, 193, 58)" @click="record.templateStatus = 0">
+                启用
+              </a-button>
+            </span>
+            <span v-else>
+              <a-button type="primary" danger @click="record.templateStatus = 1">禁用</a-button>
+            </span>
+          </template>
           <template v-if="column.key === 'operation'">
             <a-button type="primary" class="btn-manager">编辑</a-button>
             <a-button type="primary" danger>删除</a-button>
           </template>
+        </template>
+        <template #expandedRowRender>
+          <a-table :columns="innerColumns" :data-source="innertemplatedata" :pagination="false">
+            <template #bodyCell></template>
+          </a-table>
         </template>
       </a-table>
     </div>
@@ -249,13 +339,16 @@ const data = ref([
   height: 100%;
   overflow: auto;
 }
+
 .search {
   padding: 24px 24px 0 24px;
   background: #ffffff;
   border-radius: 6px;
+
   .search-item:nth-child(1) {
     margin-left: 15px;
   }
+
   .search-item:nth-child(2) {
     margin-left: 30px;
   }
@@ -267,6 +360,7 @@ const data = ref([
   display: flex;
   align-items: center;
   justify-content: right;
+
   .addModule {
     margin: 0px 20px;
   }
@@ -276,17 +370,20 @@ const data = ref([
   .tem-item {
     margin-top: 20px;
   }
+
   .tem-item:nth-child(7) {
     text-align: right;
     margin-left: 300px;
   }
 }
+
 .message-section {
   height: 100%;
   border-radius: 6px;
   margin-top: 12px;
   background: #ffffff;
   padding: 12px;
+
   .btn-manager {
     margin-right: 10px;
   }
--- deliver-front/vue/src/views/Message/type/index.ts
+++ deliver-front/vue/src/views/Message/type/index.ts
@@ -1,8 +1,78 @@
 export interface messageTemplate {
+  /**
+   * 模板id
+   */
+  templateId: string
+  /**
+   * 模板名
+   */
   templateName: string
-  pushRange: string
-  userType: string
-  channel: string
+  /**
+   * 推送范围
+   */
+  pushRange: number
+  /**
+   * 用户类型
+   */
+  usersType: number
+  // /**
+  //  * 推送方式
+  //  */
+  pushWays?: string
+  /**
+   * 渠道选择
+   */
+  channelType: number
+  /**
+   * 消息类型
+   */
   messageType: string
-  channelApp: string
+  /**
+   * 模板累计使用数
+   */
+  useCount: number
+  /**
+   * 模板状态
+   */
+  templateStatus: number
+  /**
+   * 创建用户
+   */
+  createUser: string
+  /**
+   * 创建时间
+   */
+  createTime: string
+  /**
+   * Appid
+   */
+  appId: number
+  /**
+   * App名称
+   */
+  appName: string
+}
+
+// 搜索框接口
+export interface searchMessage {
+  /**
+   * 模板名
+   */
+  templateName?: string
+  /**
+   * 推送范围
+   */
+  pushRange?: number
+  /**
+   * 用户类型
+   */
+  usersType?: number
+  /**
+   * 当前页面序号
+   */
+  currentPage: number
+  /**
+   * 页面大小
+   */
+  pageSize: number
 }
