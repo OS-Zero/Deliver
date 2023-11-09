df2df9179c9aef2b4d4c318334c95045193be71b
ruochu995 <2308850421@qq.com>
2023-11-09 20:32:59 +0800

    refactor(admin): 反序列化时间修改
    通用格式

--- deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/model/dto/response/AppSearchResponseDto.java
+++ deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/model/dto/response/AppSearchResponseDto.java
@@ -1,7 +1,9 @@
 package com.oszero.deliver.admin.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -50,5 +52,6 @@ public class AppSearchResponseDto {
     /**
      * 创建时间
      */
-    private Date createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
 }
