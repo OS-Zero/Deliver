891b5df61ad229f7b5fa161bbf0a3b4e1ee47b6b
ruochu995 <2308850421@qq.com>
2023-11-12 12:04:54 +0800

    feature(server): controller aop、RabbitMQ 消费 MDC 日志
    controller 入参、出参日志打印，RabbitMQ 消费 MDC 日志

---
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/exception/MessageException.java
@@ -0,0 +1,26 @@
+package com.oszero.deliver.server.exception;
+
+import com.oszero.deliver.server.enums.ResultEnum;
+import lombok.Getter;
+
+/**
+ * 消息链路异常
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
+@Getter
+public class MessageException extends RuntimeException {
+
+    private String message;
+    private final ResultEnum code = ResultEnum.ERROR;
+
+    public MessageException() {
+        super();
+    }
+
+    public MessageException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
