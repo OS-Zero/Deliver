40ac76bedd33dcdbe531b10d5b31975bf0a7a2f3
ruochu995 <2308850421@qq.com>
2023-11-13 14:07:11 +0800

    refactor(front-vue): 部分页面修改

--- deliver-front/vue/src/views/Channel/type/index.ts
+++ deliver-front/vue/src/views/Channel/type/index.ts
@@ -1,8 +1,26 @@
-export interface messageTemplate {
-  templateName: string
-  pushRange: string
-  userType: string
-  channel: string
-  messageType: string
-  channelApp: string
+import type { Dayjs } from 'dayjs'
+
+export interface appSearch {
+  appName: string
+  channelType: number
+  /**
+   * 当前页面序号
+   */
+  currentPage: number
+  /**
+   * 页面大小
+   */
+  pageSize: number
+  /**
+   * 起始日期
+   */
+  startTime?: string
+  /**
+   * 结束日期
+   */
+  endTime?: string
+  /**
+   * 时间段
+   */
+  perid?: [Dayjs, Dayjs]
 }
