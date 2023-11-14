b3e4d5fd1c612efd100b05e2b733b3fd20c8812d
dengyu <3233891353@qq.com>
2023-11-14 21:39:22 +0800

    style:样式修改

--- deliver-front/vue/src/router/index.ts
+++ deliver-front/vue/src/router/index.ts
@@ -50,15 +50,6 @@ const routes: RouteRecordRaw[] = [
           parent: 'home',
           name: '平台文件管理'
         }
-      },
-      {
-        path: 'setting',
-        name: 'setting',
-        component: async () => await import('@/views/Setting/index.vue'),
-        meta: {
-          parent: 'home',
-          name: '系统设置'
-        }
       }
     ]
   },
