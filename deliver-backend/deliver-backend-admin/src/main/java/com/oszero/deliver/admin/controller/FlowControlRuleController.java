f55ae56ea303df9455859ee7db8cb9cd49ecc882
ruochu995 <2308850421@qq.com>
2023-11-21 21:49:45 +0800

    feature(backend): 添加 checkstyle 配置文件

---
+++ deliver-backend/deliver-backend-admin/src/main/java/com/oszero/deliver/admin/controller/FlowControlRuleController.java
@@ -0,0 +1,18 @@
+package com.oszero.deliver.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 流控规则控制器
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
+@RestController
+@RequestMapping("/flowControlRule")
+@RequiredArgsConstructor
+public class FlowControlRuleController {
+
+}
