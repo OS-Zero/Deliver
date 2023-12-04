41fdd5d4a4202a5b29eec971f1e9fd4308a8b5d3
ruochu995 <2308850421@qq.com>
2023-12-04 10:46:38 +0800

    refactor(server): RocketMQProducer 逻辑增强

--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/producer/impl/RocketMQProducer.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/producer/impl/RocketMQProducer.java
@@ -4,10 +4,12 @@ import cn.hutool.json.JSONUtil;
 import com.oszero.deliver.server.constant.MQConstant;
 import com.oszero.deliver.server.enums.ChannelTypeEnum;
 import com.oszero.deliver.server.exception.BusinessException;
+import com.oszero.deliver.server.exception.MessageException;
 import com.oszero.deliver.server.message.producer.Producer;
 import com.oszero.deliver.server.model.dto.SendTaskDto;
 import com.oszero.deliver.server.util.RocketMQUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -21,6 +23,7 @@ import java.util.Objects;
  * @author oszero
  * @version 1.0.0
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @ConditionalOnProperty(value = "mq-type", havingValue = "rocketmq")
@@ -60,11 +63,27 @@ public class RocketMQProducer implements Producer {
                 break;
             }
         }
-        if (!Objects.isNull(sendResult)) {
-            SendStatus sendStatus = sendResult.getSendStatus();
-            if (!SendStatus.SEND_OK.equals(sendStatus)) {
-                throw new BusinessException("RocketMQ 消息发送失败！！！");
-            }
+        if (!Objects.isNull(sendResult) && Objects.equals(SendStatus.SEND_OK, sendResult.getSendStatus())) {
+            log.info("模板 ID " + sendTaskDto.getTemplateId() + "RocketMQ 消息发送成功");
+        } else {
+            // 消息再次重试发送
+            retry(sendTaskDto);
+        }
+    }
+
+    /**
+     * 重新发送消息
+     *
+     * @param sendTaskDto dto
+     */
+    public void retry(SendTaskDto sendTaskDto) {
+        if (sendTaskDto.getRetry() > 0) {
+            log.error("模板 ID " + sendTaskDto.getTemplateId() + " 再次发送消息，消息重试发送");
+            sendTaskDto.setRetry(sendTaskDto.getRetry() - 1);
+            sendTaskDto.setRetried(1);
+            sendMessage(sendTaskDto);
+        } else {
+            throw new MessageException("RocketMQ 消息发送失败，重试次数已用完，请检查 MQ 情况！！！");
         }
     }
 }
