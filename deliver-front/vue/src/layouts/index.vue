550a40f1ce8453ea479ef8f1c5a625f7834de82e
dengyu <3233891353@qq.com>
2023-12-04 15:41:28 +0800

    refractor(dashboard): 抽离echart配置文件 Closes https://gitee.com/OS-Zero/deliver/issues/I8KXJ9

--- deliver-front/vue/src/layouts/index.vue
+++ deliver-front/vue/src/layouts/index.vue
@@ -1,4 +1,8 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Header from './components/Header/index.vue'
+import Banner from '../components/Banner/index.vue'
+import Section from './components/Section/index.vue'
+</script>
 
 <template>
 	<a-layout style="min-height: 100%">
