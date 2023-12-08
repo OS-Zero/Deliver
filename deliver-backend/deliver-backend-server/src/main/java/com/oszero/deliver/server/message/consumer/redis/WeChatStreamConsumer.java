1726f3de5ea2f518ac617a4b56e833fe5b5e037c
ruochu995 <2308850421@qq.com>
2023-12-08 17:41:46 +0800

    feature(server): 所有consumer 消息链路追踪已完成

--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/consumer/redis/WeChatStreamConsumer.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/consumer/redis/WeChatStreamConsumer.java
@@ -2,7 +2,6 @@ package com.oszero.deliver.server.message.consumer.redis;
 
 import com.oszero.deliver.server.message.consumer.handler.impl.WeChatHandler;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.stream.StreamListener;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Component;
  * @author oszero
  * @version 1.0.0
  */
-@Slf4j
 @Component
 @RequiredArgsConstructor
 @ConditionalOnProperty(value = "mq-type", havingValue = "redis")
@@ -25,7 +23,6 @@ public class WeChatStreamConsumer implements StreamListener<String, ObjectRecord
 
     @Override
     public void onMessage(ObjectRecord<String, String> message) {
-        log.info("[WeChatStreamConsumer 接收到消息] {}", message);
         streamCommonConsumer.omMessageAck(message, weChatHandler);
     }
 }
--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/consumer/rocketmq/DingConsumer.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/message/consumer/rocketmq/DingConsumer.java
@@ -3,7 +3,6 @@ package com.oszero.deliver.server.message.consumer.rocketmq;
 import com.oszero.deliver.server.constant.MQConstant;
 import com.oszero.deliver.server.message.consumer.handler.impl.DingHandler;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
@@ -16,7 +15,6 @@ import org.springframework.stereotype.Component;
  * @author oszero
  * @version 1.0.0
  */
-@Slf4j
 @Component
 @RequiredArgsConstructor
 @RocketMQMessageListener(topic = MQConstant.DING_TOPIC, consumerGroup = MQConstant.DING_CONSUMER_GROUP)
@@ -34,7 +32,6 @@ public class DingConsumer implements RocketMQListener<MessageExt> {
      */
     @Override
     public void onMessage(MessageExt messageExt) {
-        log.info("[RocketMQListener]#[DingConsumer] 接收到消息");
         commonConsumer.omMessageAck(messageExt, dingHandler);
     }
 
