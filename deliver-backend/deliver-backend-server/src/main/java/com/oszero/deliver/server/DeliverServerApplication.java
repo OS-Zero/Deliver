80e1e5f5ddef70afb7a526fd8a7275daf10d7bee
ruochu995 <2308850421@qq.com>
2023-12-11 16:53:06 +0800

    feature(server): 引入 druid 数据库连接池、cache 缓存处理

--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/DeliverServerApplication.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/DeliverServerApplication.java
@@ -2,6 +2,7 @@ package com.oszero.deliver.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Deliver 消息推送平台服务端启动类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0.0
  */
 @SpringBootApplication
+@EnableCaching //开启缓存功能
 public class DeliverServerApplication {
 
     public static void main(String[] args) {
