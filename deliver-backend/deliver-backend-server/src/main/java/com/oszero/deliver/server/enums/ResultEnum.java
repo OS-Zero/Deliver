adda00ec527f51d4f5d9e36dd62cf5beef5116e8
ruochu995 <2308850421@qq.com>
2023-12-13 15:12:14 +0800

    refactor(server): 错误状态枚举，消息异常部分修改

--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/enums/ResultEnum.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/enums/ResultEnum.java
@@ -1,17 +1,20 @@
 package com.oszero.deliver.server.enums;
 
+import lombok.Getter;
+
 /**
  * 响应状态枚举
  *
  * @author oszero
  * @version 1.0.0
  */
+@Getter
 public enum ResultEnum {
     SUCCESS(200, "success"),
     CLIENT_ERROR(400, "client_error"),
     SERVER_ERROR(500, "server_error"),
-    ERROR(600, "error");
-
+    ERROR(600, "error"),
+    REFRESH_TOKEN(700, "refresh_token");
     private final Integer code;
     private final String message;
 
@@ -20,11 +23,4 @@ public enum ResultEnum {
         this.message = message;
     }
 
-    public Integer getCode() {
-        return this.code;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
 }
