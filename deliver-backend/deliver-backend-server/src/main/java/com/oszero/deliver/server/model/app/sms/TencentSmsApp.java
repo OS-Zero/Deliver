dc7b50754c3fadb4cac98f4da0f193fbef79b716
ruochu995 <2308850421@qq.com>
2023-12-14 21:50:46 +0800

    feature(server): 对接腾讯云短信Client完成

--- deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/model/app/sms/TencentSmsApp.java
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/model/app/sms/TencentSmsApp.java
@@ -1,10 +1,19 @@
 package com.oszero.deliver.server.model.app.sms;
 
+import lombok.*;
+
 /**
  * 腾讯云短信 APP 配置
  *
  * @author oszero
  * @version 1.0.0
  */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class TencentSmsApp extends SmsApp {
+    private String secretId;
+    private String secretKey;
 }
