e6be054714d1f9efaa5d12b8fed1eea27a0976b8
ruochu995 <2308850421@qq.com>
2023-12-16 16:32:26 +0800

    fix(server): 修复消息链路过程一些问题

---
+++ deliver-backend/deliver-backend-server/src/main/java/com/oszero/deliver/server/util/ThreadLocalUtils.java
@@ -0,0 +1,21 @@
+package com.oszero.deliver.server.util;
+
+import com.oszero.deliver.server.model.dto.common.SendTaskDto;
+
+/**
+ * ThreadLocal 工具类
+ *
+ * @author oszero
+ * @version 1.0.0
+ */
+public class ThreadLocalUtils {
+    private final static ThreadLocal<SendTaskDto> SEND_TASK_DTO_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static void setSendTaskDto(SendTaskDto sendTaskDto) {
+        SEND_TASK_DTO_THREAD_LOCAL.set(sendTaskDto);
+    }
+
+    public static SendTaskDto getSendTaskDto() {
+        return SEND_TASK_DTO_THREAD_LOCAL.get();
+    }
+}
