4fdcaa0c94e6764a57bb90bc158745dcf7dc4249
DY <3233891353@qq.com>
2024-12-10 11:02:42 +0800

    feat: 添加表单项校验逻辑

--- deliver-front/vue/src/utils/validate.ts
+++ deliver-front/vue/src/utils/validate.ts
@@ -4,3 +4,8 @@ export async function validateList(_rule: any, value: any) {
 	}
 	return Promise.reject('请添加至少一个用户')
 }
+
+export async function validateEmail(_r, value: string) {
+	const regExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+	return regExp.test(value) ? Promise.resolve() : Promise.reject('邮箱格式错误')
+}
