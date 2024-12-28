f2415ba9b4d0acf589125d963147ebd74181974c
DY <3233891353@qq.com>
2024-12-28 09:17:07 +0800

    fix: 修复请求拦截器错误

--- deliver-front/vue/src/utils/request.ts
+++ deliver-front/vue/src/utils/request.ts
@@ -1,36 +1,38 @@
-import axios from 'axios'
-import { message } from 'ant-design-vue'
+import axios from 'axios';
+import { message } from 'ant-design-vue';
 const service = axios.create({
 	baseURL: '/backend',
 	timeout: 20000,
-})
+});
 
 service.interceptors.request.use(
 	(config) => {
-		config.headers['FrontPlatform'] = 'vue'
-		config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('access_token')
-		return config
+		config.headers['FrontPlatform'] = 'vue';
+		config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('access_token');
+		return config;
 	},
 	(err) => {
-		message.error(err)
-		return Promise.reject(err)
+		message.error(err);
+		return Promise.reject(err);
 	},
-)
+);
 
 service.interceptors.response.use(
 	(res) => {
-		const _res = res.data
-		if (_res?.data.code === 600) {
-			message.error(_res?.data.errorMessage)
-			return Promise.reject(_res?.data.errorMessage)
+		const _res = res.data;
+		console.log(_res);
+
+		if (_res.code === 600) {
+			message.error(_res.errorMessage);
+			return Promise.reject(_res.errorMessage);
 		}
-		if (res?.data.code === 200) return _res.data
-		message.error('请求错误')
-		return Promise.reject('请求错误')
+		if (_res.code === 200) return _res.data;
+		message.error('请求错误');
+		return Promise.reject('请求错误');
 	},
 	(err) => {
-		message.error(err)
-		return Promise.reject(err)
+		message.error(err);
+		return Promise.reject(err);
 	},
-)
-export default service
+);
+export default service;
