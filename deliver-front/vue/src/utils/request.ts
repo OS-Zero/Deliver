d62e1e2f5a9ca3dbc4f97de5ac80a56f2add44d6
DY <3233891353@qq.com>
2025-01-09 23:31:12 +0800

    feat: 回退

--- deliver-front/vue/src/utils/request.ts
+++ deliver-front/vue/src/utils/request.ts
@@ -24,7 +24,6 @@ service.interceptors.response.use(
 	(res) => {
 		if (res.status === 200) {
 			const _res = res.data;
-			return _res.data;
 			if (_res.code === 0) return _res.data;
 			if (_res.code === 1) {
 				message.error(_res.errorMessage);
