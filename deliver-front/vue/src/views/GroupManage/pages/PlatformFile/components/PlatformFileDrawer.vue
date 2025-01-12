f3089ee65d7b9ac8fee662d2f3681f19e1a061d4
DY <3233891353@qq.com>
2025-01-12 14:11:39 +0800

    feat: 重构平台文件代码

---
+++ deliver-front/vue/src/views/GroupManage/pages/PlatformFile/components/PlatformFileDrawer.vue
@@ -0,0 +1,83 @@
+<script lang="ts" setup>
+import { DrawerProps } from '@/types/components';
+import { dynamic, getDataFromSchema } from '@/utils/utils';
+import { message } from 'ant-design-vue';
+import { ref, reactive, onUnmounted, watch } from 'vue';
+import { platformFileLocale, platformFileSchema, platformFileSchemaDeps } from '@/config/platformFile';
+import { updateChannelApp } from '@/api/channelApp';
+type Operation = 'upload' | 'more'
+const props = defineProps<{
+	open: boolean
+	operation: Operation
+	record: Record<string, any>
+}>()
+const emit = defineEmits(['close'])
+const formRef = ref()
+
+const titleList: Record<Operation, string> = {
+	upload: '上传文件',
+	more: '文件详情'
+}
+
+const drawerState = reactive<DrawerProps>({
+	open: props.open,
+	title: titleList[props.operation],
+	placement: props.operation === 'more' ? 'left' : 'right',
+	extra: props.operation === 'more' ? false : true,
+})
+watch(props, (newProps) => {
+	Object.assign(drawerState, {
+		open: newProps.open,
+		title: titleList[newProps.operation],
+		placement: newProps.operation === 'more' ? 'left' : 'right',
+		extra: props.operation === 'more' ? false : true,
+	});
+	newProps.operation === 'more' && initMoreDate();
+})
+
+const { dynamicData: platformFileForm, stop } = dynamic(platformFileSchema, platformFileSchemaDeps)
+const initMoreDate = () => {
+	const set = new Set(['channelType', 'appId', 'platformFileType'])
+	const arr: Array<{ label: string; value: any }> = []
+	for (const key in props.record) {
+		if (!set.has(key)) {
+			arr.push({
+				label: platformFileLocale[key],
+				value: props.record[key]
+			})
+		}
+	}
+	Object.assign(moreInfo, arr)
+}
+const moreInfo = reactive<Array<{ label: string; value: any }>>([])
+const operationDispatch = {
+	upload: async () => {
+		await formRef.value.validate()
+		await updateChannelApp(getDataFromSchema(platformFileForm))
+		message.success('上传成功')
+		handleCancel()
+	},
+}
+
+const handleCancel = () => {
+	props.operation !== 'more' && formRef.value.resetFields()
+	drawerState.open = false
+	emit('close')
+}
+onUnmounted(() => {
+	stop()
+})
+
+</script>
+
+<template>
+	<Drawer v-bind="drawerState" @ok="operationDispatch[operation]" @close="handleCancel">
+		<Form ref="formRef" v-if="operation === 'upload'" :form-schema="platformFileForm" />
+		<div v-else-if="operation === 'more'">
+			<Descriptions :data="moreInfo" :config="{ column: 1 }">
+			</Descriptions>
+		</div>
+	</Drawer>
+</template>
+
+<style lang="scss" scoped></style>
--- deliver-front/vue/src/views/GroupManage/pages/PlatformFile/index.vue
+++ deliver-front/vue/src/views/GroupManage/pages/PlatformFile/index.vue
@@ -3,16 +3,16 @@ import { ref, onBeforeMount, reactive, h, onUnmounted, watch } from 'vue'
 import { CopyOutlined, DownOutlined, FilterOutlined, CloseOutlined } from '@ant-design/icons-vue';
 import { copyToClipboard, dynamic, getDataFromSchema } from '@/utils/utils';
 import { FormInstance, message } from 'ant-design-vue';
-import Drawer from '@/components/Drawer/index.vue'
 import SearchInput from '@/components/SearchInput/index.vue'
 import { debounce } from 'lodash'
 import { usePagination } from '@/hooks/table';
-import { getPlatformFile, uploadPlatformFile } from '@/api/platformFile';
+import { getPlatformFile } from '@/api/platformFile';
 import { PlatformFile } from '@/types/platformFile';
-import { platformFileSchema, platformFileSchemaDeps, filterSchema, filterSchemaMaps, platformFileLocale, platformFileColumns } from '@/config/platformFile';
+import { filterSchema, filterSchemaMaps, platformFileColumns } from '@/config/platformFile';
+import PlatformFileDrawer from './components/PlatformFileDrawer.vue';
+type Operation = 'upload' | 'more'
 const dataSource = ref<PlatformFile[]>([])
-const { dynamicData: platformFileForm, stop } = dynamic(platformFileSchema, platformFileSchemaDeps)
-const { dynamicData: filterForm, stop: stopFilterDynamic } = dynamic(filterSchema, filterSchemaMaps)
+const { dynamicData: filterForm, stop } = dynamic(filterSchema, filterSchemaMaps)
 const searchValue = ref('')
 const handleSearch = async () => {
 	const { records, total } = await getPlatformFile({ platformFileName: searchValue.value, ...getDataFromSchema(filterForm), pageSize: pagination.pageSize, currentPage: pagination.current })
@@ -20,11 +20,10 @@ const handleSearch = async () => {
 	pagination.total = total
 }
 const debounceSearch = debounce(handleSearch, 200)
-const { pagination } = usePagination(handleSearch)
+const { pagination, resetPagination } = usePagination(handleSearch)
 watch(filterForm, () => {
 	debounceSearch()
 })
-const moreInfo = reactive<Array<{ label: string; value: any }>>([])
 const copyId = async (text: string) => {
 	try {
 		await copyToClipboard(text)
@@ -35,81 +34,34 @@ const copyId = async (text: string) => {
 }
 const drawerState = reactive<{
 	open: boolean
-	title: string
-	operation: string
-	placement: 'left' | 'right' | 'top' | 'bottom' | undefined
-	extra: boolean
+	operation: 'upload' | 'more'
+	record: Record<string, any>
 }>({
 	open: false,
-	title: '',
-	operation: '',
-	placement: "right",
-	extra: true
+	operation: 'upload',
+	record: {}
 })
 const filterState = reactive({
 	open: false
 })
-
-
 const formRef = ref<FormInstance>();
-const handleActions = async (action: 'upload' | 'more', record?: Record<string, any>) => {
-	drawerState.placement = 'right'
-	if (action === 'upload') {
-		drawerState.title = '上传文件'
-		drawerState.open = true
-		drawerState.operation = 'upload'
-	} else if (action === 'more') {
-		drawerState.title = '文件详情'
-		drawerState.operation = 'showMore'
-		drawerState.placement = 'left'
-		drawerState.extra = false
-		drawerState.open = true
-		const set = new Set(['channelType', 'appId', 'platformFileType'])
-		const arr: Array<{ label: string; value: any }> = []
-		for (const key in record) {
-			if (!set.has(key)) {
-				arr.push({
-					label: platformFileLocale[key],
-					value: record[key]
-				})
-			}
-		}
-		Object.assign(moreInfo, arr)
-	}
-}
-const handleDrawer = {
-	ok: async () => {
-		try {
-			await formRef.value?.validate()
-			if (drawerState.operation === 'upload') {
-				await uploadPlatformFile(getDataFromSchema(platformFileForm))
-				message.success('上传成功')
-			} else if (drawerState.operation === 'more') {
-				drawerState.placement = 'right'
-			}
-			drawerState.open = false
-			formRef.value?.resetFields()
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	cancel: () => {
-		drawerState.open = false
-		drawerState.extra = true
-		formRef.value?.resetFields()
-	}
+const handleActions = async (operation: Operation, record: Record<string, any> = {}) => {
+	(operation === 'upload' || operation === 'more' || operation === 'more') && (drawerState.operation = operation, drawerState.open = true);
+	(operation === 'more') && (drawerState.record = record);
 }
 const handleFilterClose = () => {
 	filterState.open = false
 	formRef.value?.resetFields()
 }
+const handleDrawerClose = () => {
+	drawerState.open = false;
+	drawerState.operation === 'upload' && resetPagination() && handleSearch();
+}
 onBeforeMount(() => {
 	handleSearch()
 })
 onUnmounted(() => {
-	//停止监听动态数据
 	stop()
-	stopFilterDynamic()
 })
 </script>
 
@@ -158,16 +110,7 @@ onUnmounted(() => {
 			<template #extra><a-button type="text" :icon="h(CloseOutlined)" @click="handleFilterClose"></a-button></template>
 			<Form ref="formRef" :form-schema="filterForm" />
 		</a-card>
-		<Drawer :placement="drawerState.placement" :open="drawerState.open" :title="drawerState.title"
-			:extra="drawerState.extra" @ok="handleDrawer.ok" @close="handleDrawer.cancel">
-			<Form v-if="drawerState.operation === 'upload'" ref="formRef" :label-col="{ span: 7 }"
-				:form-schema="platformFileForm" />
-			<div v-else-if="drawerState.operation === 'showMore'">
-				<a-descriptions :column="1">
-					<a-descriptions-item v-for="item in moreInfo" :label="item.label">{{ item.value }}</a-descriptions-item>
-				</a-descriptions>
-			</div>
-		</Drawer>
+		<PlatformFileDrawer v-bind="drawerState" @close="handleDrawerClose"></PlatformFileDrawer>
 	</div>
 </template>
 
