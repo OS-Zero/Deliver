613c04e75cbc33ef6c4cb9604af1f1497faa17cf
若初995 <2308850421@qq.com>
2025-02-15 19:31:01 +0800

    refactor(admin): 添加 admin 服务标识
    在 DeliverAdminApplication 类的 main 方法中添加注释，明确标识这是一个 admin 服务。这样做的目的是为了提高代码的可读性和可维护性，方便其他开发人员快速理解该类的用途。
    
    - 在 main 方法中添加注释 "// 这是admin服务"
    -通过这种方式，其他开发人员可以快速识别该类的用途

--- deliver-backend/deliver-application/deliver-application-admin/src/main/java/com/oszero/application/admin/DeliverAdminApplication.java
+++ deliver-backend/deliver-application/deliver-application-admin/src/main/java/com/oszero/application/admin/DeliverAdminApplication.java
@@ -28,6 +28,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeliverAdminApplication {
 
     public static void main(String[] args) {
+        // 这是admin服务
         SpringApplication.run(DeliverAdminApplication.class, args);
     }
 }
